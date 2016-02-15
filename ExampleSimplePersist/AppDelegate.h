ca8894febc607c8c3b65e52653b45158db755d7e
José Carlos <josecarlosestela@gmail.com>
2016-02-15 13:00:43 +0100

    Initial Commit

---
+++ ExampleSimplePersist/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ExampleSimplePersist
+//
+//  Created by José Carlos on 15/2/16.
+//  Copyright © 2016 José Carlos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ExampleSimplePersist/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ExampleSimplePersist
+//
+//  Created by José Carlos on 15/2/16.
+//  Copyright © 2016 José Carlos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
